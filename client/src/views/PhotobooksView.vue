4619c3aa57d4c63103467bcb04a9e75a1825bf13
GuysKapen <guyskapen@gmail.com>
2022-10-25 11:38:31 +0700

    Add dynamic add pages when creating photobook

--- client/src/views/PhotobooksView.vue
+++ client/src/views/PhotobooksView.vue
@@ -63,9 +63,18 @@
         </div>
         <label class="block text-sm font-medium text-gray-700 mt-4"> Pages </label>
         <div class="mt-2 ml-4" v-for="(page, idx) in pages" :key="idx">
-          <div class="">
-            <label class="block text-sm font-medium text-gray-700"> Content </label>
-            <input class="
+          <div class="my-2 flex justify-between">
+            <p class="text-gray-700 text-sm">Page {{idx + 1}}</p>
+            <button @click="page['collapse'] = !page['collapse']">
+              <span class="material-symbols-outlined mr-2 mt-[0.1rem]">
+                {{page['collapse'] ? 'arrow_drop_down' : 'arrow_drop_up'}}
+              </span>
+            </button>
+          </div>
+          <div v-if="!page['collapse']">
+            <div class="">
+              <label class="block text-sm font-medium text-gray-700"> Content </label>
+              <input class="
               mt-1
               focus:ring-indigo-500 focus:border-indigo-500
               block
@@ -75,13 +84,13 @@
               border-gray-300
               rounded-md
             " placeholder="Name..." type="text" v-model="page.content" />
-          </div>
+            </div>
 
-          <div class="mt-4">
-            <label class="block text-sm font-medium text-gray-700">
-              Cover photo
-            </label>
-            <div class="
+            <div class="mt-4">
+              <label class="block text-sm font-medium text-gray-700">
+                Cover photo
+              </label>
+              <div class="
               mt-1
               flex
               justify-center
@@ -91,15 +100,15 @@
               border-2 border-gray-300 border-dashed
               rounded-md
             ">
-              <div class="space-y-1 text-center">
-                <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48"
-                  aria-hidden="true">
-                  <path
-                    d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-                <div class="flex text-sm text-gray-600">
-                  <label :for="`cover-${idx}`" class="
+                <div class="space-y-1 text-center">
+                  <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48"
+                    aria-hidden="true">
+                    <path
+                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                      stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                  </svg>
+                  <div class="flex text-sm text-gray-600">
+                    <label :for="`cover-${idx}`" class="
                     relative
                     cursor-pointer
                     bg-white
@@ -112,20 +121,21 @@
                     focus-within:ring-offset-2
                     focus-within:ring-indigo-500
                   ">
-                    <span>Upload a file</span>
-                    <input :id="`cover-${idx}`" :name="`cover-${idx}`" type="file" class="sr-only"
-                      v-on:change="onPageImageSelected($event, idx)" />
-                  </label>
-                  <p class="pl-1">or drag and drop</p>
+                      <span>Upload a file</span>
+                      <input :id="`cover-${idx}`" :name="`cover-${idx}`" type="file" class="sr-only"
+                        v-on:change="onPageImageSelected($event, idx)" />
+                    </label>
+                    <p class="pl-1">or drag and drop</p>
+                  </div>
+                  <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
                 </div>
-                <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
               </div>
             </div>
-          </div>
 
+          </div>
         </div>
         <div class="text-indigo-600 flex items-center mt-2">
-          <button class="text-sm flex items-center leading-3">
+          <button class="text-sm flex items-center leading-3" @click="addPage">
             <span class="material-symbols-outlined text-sm mr-2 mt-[0.1rem]">
               add
             </span>
@@ -206,7 +216,7 @@ export default {
     cover: null,
     serverUrl,
     imgUrlFor,
-    pages: [{ content: null, image: null }]
+    pages: []
   }),
   methods: {
     onImageSelected(e) {
@@ -311,6 +321,9 @@ export default {
         console.error("Create photobook", error);
       }
     },
+    addPage() {
+      this.pages = [...this.pages, { image: null, content: null, collapse: false }]
+    }
   },
   computed: {
     ...mapState(useAuthStore, ["user"]),
